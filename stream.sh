79ffb9a2c794b535c2e01e884a10da4cd213bc05
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-31 03:29:23 -0700

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# Define the output directory and file name
 OUTPUT_DIR="/var/www/html/videos"
-OUTPUT_FILE="stream.3gp"
+OUTPUT_FILE="stream.mp4"
 
-# Ensure the output directory exists
-mkdir -p "$OUTPUT_DIR"
-
-# Infinite loop to keep the streaming and transcoding process running
 while true
 do
-    ffmpeg -re -i "http://ktv.im:8080/44444/44444/81825" \
-        -c:v h263 -b:v 70k -r 15 -s 176x144 \
+    ffmpeg -re -i "https://video.yementdy.tv/hls/yementoday.m3u8" \
+        -c:v h263 -b:v 70k -r 15 -vf scale=176:144 \
         -c:a aac -b:a 32k -ac 1 -ar 32000 \
-        -f 3gp "$OUTPUT_DIR/$OUTPUT_FILE"
+        -f mp4 "$OUTPUT_DIR/$OUTPUT_FILE"
 
-    # Wait for 5 seconds before restarting the process in case of failure
     sleep 5
 done
