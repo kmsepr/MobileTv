913f2def08f7d592dc31d887db18bc6ef61d488c
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-31 03:43:58 -0700

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 OUTPUT_DIR="/var/www/html/videos"
-OUTPUT_FILE="stream.mp4"
+OUTPUT_FILE="stream.3gp"
 
 while true
 do
     ffmpeg -re -fflags +genpts -i "https://video.yementdy.tv/hls/yementoday.m3u8" \
         -c:v h263 -b:v 70k -r 15 -vf scale=176:144 \
-        -c:a aac -b:a 32k -ac 1 -ar 32000 \
-        -movflags +faststart -f mp4 "$OUTPUT_DIR/$OUTPUT_FILE"
+        -c:a libopencore_amrnb -b:a 12.2k -ac 1 -ar 8000 \
+        -f 3gp "$OUTPUT_DIR/$OUTPUT_FILE"
 
     sleep 5
 done
