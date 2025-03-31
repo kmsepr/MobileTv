bfa6efd648eec55f885826bea57c747ca2f7d488
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-31 03:05:32 -0700

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 OUTPUT_DIR="/var/www/html/videos"
-OUTPUT_FILE="stream.mp4"
+OUTPUT_FILE="stream.3gp"
 
 while true
 do
     ffmpeg -re -i "https://edge1.1internet.tv/dash-live2/streams/1tv-dvr/1tvdash.mpd" \
         -map 0:v:3 -map 0:a:1 \
-        -acodec amr_wb -ar 16000 -ac 1 \
-        -vcodec h263 -vb 70k -r 15 -vf scale=176:144 \
-        -f mp4 "$OUTPUT_DIR/$OUTPUT_FILE"
+        -c:v h263 -b:v 70k -r 15 -vf scale=176:144 \
+        -c:a aac -b:a 32k -ar 32000 -ac 1 \
+        -f 3gp "$OUTPUT_DIR/$OUTPUT_FILE"
 
     sleep 5
 done
