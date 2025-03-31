113b026be906623cea3f08b2c097b72bfb0f7a23
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-31 14:59:35 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+OUTPUT_DIR="/var/www/html/videos"
+OUTPUT_FILE="stream.mp4"
+
+while true
+do
+    ffmpeg -re -i "https://edge1.1internet.tv/dash-live2/streams/1tv-dvr/1tvdash.mpd" \
+        -map 0:v:3 -map 0:a:1 \
+        -acodec amr_wb -ar 16000 -ac 1 \
+        -vcodec h263 -vb 70k -r 15 -vf scale=176:144 \
+        -f mp4 "$OUTPUT_DIR/$OUTPUT_FILE"
+
+    sleep 5
+done
