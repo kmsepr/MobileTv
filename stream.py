4eb3ae4692b11bfd48deff3e8ccc6eaa9ba55505
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-31 07:55:25 -0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -30,7 +30,8 @@ def generate_stream(url):
         "-reconnect_delay_max", "10", 
         "-fflags", "+genpts+nobuffer",  # Generate PTS and prevent buffer loops
         "-flags", "low_delay", 
-        "-i", url,  
+        "-i", url,
+        "-vn",
         "-ac", "1", 
         "-b:a", "40k", 
         "-bufsize", "4096k",  # Keep at 4096k since you found it better
