8f3841fa814790ed7bfb0b689764577d1f455981
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-31 03:14:17 -0700

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+# Define the output directory and file name
 OUTPUT_DIR="/var/www/html/videos"
 OUTPUT_FILE="stream.3gp"
 
+# Ensure the output directory exists
+mkdir -p "$OUTPUT_DIR"
+
+# Infinite loop to keep the streaming and transcoding process running
 while true
 do
-    ffmpeg -re -i "https://edge1.1internet.tv/dash-live2/streams/1tv-dvr/1tvdash.mpd" \
-        -map 0:v:3 -map 0:a:1 \
-        -c:v h263 -b:v 70k -r 15 -vf scale=176:144 \
-        -c:a aac -b:a 32k -ar 32000 -ac 1 \
+    ffmpeg -re -i "http://ktv.im:8080/44444/44444/81825" \
+        -c:v h263 -b:v 70k -r 15 -s 176x144 \
+        -c:a aac -b:a 32k -ac 1 -ar 32000 \
         -f 3gp "$OUTPUT_DIR/$OUTPUT_FILE"
 
+    # Wait for 5 seconds before restarting the process in case of failure
     sleep 5
 done
