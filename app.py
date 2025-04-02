dc67d2f4acb0c169cfc9047825489942aaf945a2
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-04-02 18:37:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_youtube_audio_url(youtube_url):
     """Extracts direct audio stream URL from YouTube Live."""
     try:
         command = [
-            "/opt/venv/bin/yt-dlp",  # Adjust if needed
+            "/usr/local/bin/yt-dlp", 
             "--cookies", "/mnt/data/cookies.txt",
             "--force-generic-extractor",
             "-f", "91",  # Audio format
