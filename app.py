633d8435a18bc425b88330c6eac3f8b7be9d2eae
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-06-19 14:10:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ app = Flask(__name__)
 
 # 📡 List of YouTube Live Streams
 YOUTUBE_STREAMS = {
+     "asianet_news": "https://www.youtube.com/@asianetnews/live",
+
+
     "media_one": "https://www.youtube.com/@MediaoneTVLive/live",
     "shajahan_rahmani": "https://www.youtube.com/@ShajahanRahmaniOfficial/live",
     "qsc_mukkam": "https://www.youtube.com/c/quranstudycentremukkam/live",
