c7d726cddffd70292284a3d02536f42d3c816563
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-08-11 06:44:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def generate_stream(url):
                 "-vn",
                 "-ac", "1",
                 "-ar", "22050",
-                "-b:a", "32k",
+                "-b:a", "24k",
                 "-bufsize", "64k",
                 "-f", "mp3", "-"
             ],
