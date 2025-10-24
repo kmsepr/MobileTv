e058000ae60978a03be58c06f901b9f332afe6fd
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-10-25 00:42:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,17 +16,11 @@ TV_STREAMS = {
     "dd_malayalam": "https://d3eyhgoylams0m.cloudfront.net/v1/manifest/93ce20f0f52760bf38be911ff4c91ed02aa2fd92/ed7bd2c7-8d10-4051-b397-2f6b90f99acb/562ee8f9-9950-48a0-ba1d-effa00cf0478/2.m3u8",
     "mazhavil_manorama": "https://yuppmedtaorire.akamaized.net/v1/master/a0d007312bfd99c47f76b77ae26b1ccdaae76cb1/mazhavilmanorama_nim_https/050522/mazhavilmanorama/playlist.m3u8",
     "victers_tv": "https://932y4x26ljv8-hls-live.5centscdn.com/victers/tv.stream/chunks.m3u8",
-    "bloomberg_tv": "https://bloomberg-bloomberg-3-br.samsung.wurl.tv/manifest/playlist.m3u8",
+    
     "france_24": "https://live.france24.com/hls/live/2037218/F24_EN_HI_HLS/master_500.m3u8",
-    "aqsa_tv": "http://167.172.161.13/hls/feedspare/6udfi7v8a3eof6nlps6e9ovfrs65c7l7.m3u8",
+    
     "mult": "http://stv.mediacdn.ru/live/cdn/mult/playlist.m3u8",
-    "yemen_today": "https://video.yementdy.tv/hls/yementoday.m3u8",
-    "yemen_shabab": "https://starmenajo.com/hls/yemenshabab/index.m3u8",
-    "al_sahat": "https://assahat.b-cdn.net/Assahat/assahatobs/index.m3u8",
-
-"asianet_news": "asianetnews.vgcdn.net/ptnr-Embed/v1/manifest/611d79b11b77e2f571934fd80ca1413453772ac7/1c19363c-e4a0-4e4d-ba28-771d5615b88e/54358811-f59d-4150-b8bc-c2e8714b953c/0.m3u8",
-
-"asianet_movies": "https://live.dinesh29.com.np/stream/jiotvplus/asianetmovieshd/master.m3u8",
+     
 }
 
 
