f3ae3aec3e7229cc286cca95524bb0756f20d487
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-12-08 11:44:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ TV_STREAMS = {
     "mult": "http://stv.mediacdn.ru/live/cdn/mult/playlist.m3u8",
     "yemen_today": "https://video.yementdy.tv/hls/yementoday.m3u8",
     "yemen_shabab": "https://starmenajo.com/hls/yemenshabab/index.m3u8",
-    "al_sahat": "https://assahat.b-cdn.net/Assahat/assahatobs/index.m3u8",
+    "asianet_news": "https://asianetnews.vgcdn.net/ptnr-Embed/v1/manifest/611d79b11b77e2f571934fd80ca1413453772ac7/1c19363c-e4a0-4e4d-ba28-771d5615b88e/9bd4a057-456d-43de-b887-7b87b021f1cc/3.m3u8",
 }
 
 # -----------------------
